e870ceb9b4df5affed4670d94295d3d71786f7e2
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2026-06-09 12:10:21 +0530

    Selection Sort

---
+++ algorithms/SelectionSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for (int i=0; i<n-1; i++) {
+            int minIdx = i;
+            for (int j=i+1; j<n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 4, 8, 11};
+        int[] brr = {9, 6, 4, 2, 0};
+        sort(arr);
+        sort(brr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(brr));
+    }
+}
